7a245012c3c0b2841a142c60a3e66ae103382162
John Chidiac <johnchidiac@seneca.local>
2023-11-20 09:57:48 -0500

    Fix for initial blocker (missing file extension - duh) and completion through adding css

--- app.py
+++ app.py
@@ -1,22 +1,36 @@
-from flask import Flask, request, render_template
+
+from flask import Flask, request, render_template, jsonify
 from flask_debugtoolbar import DebugToolbarExtension
 import stories
 
 app = Flask(__name__)
 app.debug = True
-app.config['SECRET_KEY'] = 'bethechange'
+app.config['SECRET_KEY'] = 999
 toolbar = DebugToolbarExtension(app)
 
 @app.route('/')
+def show_story_select():
+    """Show a dropdown menu to select the story to use"""
+    titles = [story['title'] for story in stories.mad_libs]
+    return render_template('select.jinja-html', titles=titles)
+
+@app.route('/form', methods=["POST"])
 def show_form():
     """Render Madlibs form"""
+    story_id = int(request.form['title'])
+    selected_story = stories.mad_libs[story_id]
+    # breakpoint()
+    return render_template('form.jinja-html', story_id=story_id, prompts=selected_story['prompts'])
 
-    return render_template('form')
-    
-
-@app.route('/madlib')
-def generate_madlib():
-    """Render Madlib with submitted words"""
-    
-    verb = "ran"
-    return render_template('madlib', verb=verb)
+@app.route('/story', methods=["POST"])
+def generate_story():
+    """Render story with submitted words"""
+    data = request.form
+    data = data.to_dict()
+    new_story_id = int(data['story_id'])
+    (k := next(iter(data)), data.pop(k)) # Remove first item from dictionary (story_id) as it is not used in generating story
+    new_story_prompts = stories.mad_libs[new_story_id]['prompts']
+    new_story_template = stories.mad_libs[new_story_id]['story']
+    new_story = stories.Story(new_story_prompts, new_story_template)
+    new_story_text = new_story.generate(data)
+    return render_template('madlib.jinja-html', story=new_story_text)
--- stories.py
+++ stories.py
@@ -23,6 +23,7 @@ class Story:
 
         self.prompts = words
         self.template = text
+        print(self.template)
 
     def generate(self, answers):
         """Substitute answers into text."""
@@ -43,3 +44,34 @@ story = Story(
     """Once upon a time in a long-ago {place}, there lived a
        large {adjective} {noun}. It loved to {verb} {plural_noun}."""
 )
+
+
+mad_libs = [
+    {
+        "title": "The Mysterious Adventure",
+        "prompts": ["adjective", "noun", "animal", "object", "plural_noun", "place", "silly_word"],
+        "story": "Once upon a time, in a {adjective} kingdom far away, a {noun} decided to go on an adventure. They packed their {noun} and set off into the {adjective} forest. Along the way, they met a {adjective} {animal}. The {animal} gave them a {adjective} {object} that was said to grant {plural_noun}. But to activate it, they first had to find the {adjective} {place} and say the magic words, '{silly_word} {silly_word}!'"
+    },
+    {
+        "title": "The Inventors Big Day",
+        "prompts": ["adjective", "noun", "verb", "profession", "event", "fictional_city_name", "name"],
+        "story": "In the bustling city of {fictional_city_name}, there lived an inventor named {name}. This inventor was known for their {adjective} inventions. One day, they created a {noun} that could {verb}. The news spread quickly, and soon, a {profession} came to see it. Impressed, the {profession} said, 'This is the most {adjective} thing I've seen! We must show it at the {event}!'"
+    },
+    {
+        "title": "The Space Mission",
+        "prompts": ["adjective", "made_up_planet_name", "futuristic_sounding_name", "noun", "verb", "space_creature", "plural_noun", "name"],
+        "story": "Captain {name} and their crew were about to embark on a mission to the {adjective} planet of {made_up_planet_name}. Their spaceship, the {futuristic_sounding_name}, was equipped with a {adjective} engine and a {noun} that could {verb}. As they approached the planet, they encountered a {adjective} {space_creature}. The creature asked for {plural_noun} in exchange for the secret of the planet's {adjective} {noun}."
+    },
+    {
+        "title": "The Magical School",
+        "prompts": ["adjective", "magical_subject", "last_name", "noun", "animal", "made_up_school_name", "silly_phrase"],
+        "story": "In a world where magic is real, there's a school for {adjective} wizards and witches called {made_up_school_name}. The most popular class is {magical_subject}, taught by Professor {last_name}. One day, the students were learning how to turn a {noun} into a {noun}. However, something went wrong, and instead, they turned it into a {adjective} {animal}. To fix it, they needed a {noun} and a spell that goes, '{silly_phrase}!'"
+    },
+    {
+        "title": "The Ultimate Cooking Contest",
+        "prompts": ["adjective", "food", "mysterious_ingredient", "kitchen_tool", "name"],
+        "story": "Chef {name} was competing in the {adjective} Cooking Contest. Their signature dish was a {adjective} {food}. The secret ingredient was {mysterious_ingredient}. As they started cooking, they realized they forgot their {kitchen_tool}. Luckily, a fellow chef lent them a {adjective} {kitchen_tool}. In the end, the judges called their dish '{adjective} and {adjective}!'"
+    }
+]
+
+
